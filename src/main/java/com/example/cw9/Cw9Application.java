89daf20039262f5ec0bd5199607f4bb1f27e89ca
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-05-27 11:28:03 +0600

    git init

---
+++ src/main/java/com/example/cw9/Cw9Application.java
@@ -0,0 +1,13 @@
+package com.example.cw9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw9Application.class, args);
+    }
+
+}
